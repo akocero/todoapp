41902f718463516565e9efa7ce660fcef7276d22
akocero <akocero@gmail.com>
2020-12-22 22:09:21 +0800

    Change title fot navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     const { todos } = useContext(TodoContext);
     return (
         <div className="navbar">
-            <h1>Todo List</h1>
+            <h1>Todo List Updated</h1>
             <p>Currently you have {todos.length} Todos...</p>
 
         </div>
